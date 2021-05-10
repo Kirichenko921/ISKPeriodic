b5765c040bae874e31c013e02df4088a6d3bc986
Kirichenko Alexander <050780@inbox.ru>
2021-05-10 13:25:51 +0300

    Добавьте файлы проекта.

---
+++ ISKPeriodic/ISKPeriodic.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include<vector>
+
+bool IskPeriodic(std::string text, int k)
+{
+	if (text.size() % k != 0) //если строка не кратна индексу кратности возвращаем false
+	{
+		return false;
+	}
+	std::vector<int>  textASCII; // массив для преобразования входящей строки в int
+	std::vector<int> pattern;  // массив для подстроки
+
+	for (size_t i = 0; i < text.size(); ++i) // заполняем массив int значениями соответствующих символам по таблице ASCII
+	{
+		textASCII.push_back(static_cast<int>(text[i]));
+	}
+	for (int i = 0; i < k; ++i) // присваиваем первые k значения входящей строки подстроке
+	{
+		pattern.push_back(textASCII[i]);
+	}
+
+	for (size_t itext = 0; itext < text.size(); itext += k) // проверяем каждую часть строки 
+	{
+		for (int ipattern = 0; ipattern < k; ++ipattern)    //равную подстроке
+		{
+			if (textASCII[itext + ipattern] != pattern[ipattern]) // если символ части строки не совпадает с символом подстроки
+			{
+				return false;
+			}
+		}
+	}
+	return true; // если прошли все испытания
+}
---
+++ ISKPeriodic/Source.cpp
@@ -0,0 +1,16 @@
+﻿#include"ISKPeriodic.h"
+
+int main()
+{
+
+	std::string text1 = "abcabcabcabc";
+	std::string text2 = "abcabcabcab";
+	std::string text3 = "abcabcabcabd";
+	std::string text4 = "abcababcababcab";
+	std::cout << "text1 K=3 " << IskPeriodic(text1, 3) << std::endl;
+	std::cout << "text2 K=3 " << IskPeriodic(text2, 3) << std::endl;
+	std::cout << "text3 K=3 " << IskPeriodic(text3, 3) << std::endl;
+	std::cout << "text4 K=5 " << IskPeriodic(text4, 5) << std::endl;
+	std::cout << "text4 K=4 " << IskPeriodic(text4, 4) << std::endl;
+	return 0;
+}
